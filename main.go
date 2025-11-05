02ff4b100aa0dbc9c8277c3e891dcdc17f1ccd81
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-05 14:33:09 +0700

    Feat: Load configuration and log server startup details

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"backend1/config"
 	"backend1/routes"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	cfg := config.LoadConfig()
+
+	log.Printf("Starting server on port %s", cfg.Port)
+	log.Printf("Show Password Hash: %v", cfg.ShowPasswordHash)
+
 	r := gin.Default()
 
 	routes.SetupRoutes(r)
 
-	r.Run(":8080")
+	if err := r.Run(":" + cfg.Port); err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
 }
