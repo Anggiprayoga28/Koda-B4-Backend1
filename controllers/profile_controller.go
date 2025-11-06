523e1d4a4d1e17c1889354d5540f280f8c2c63db
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-06 17:28:52 +0700

    Fix: Add JWT auth context info and security docs on profile endpoints

--- controllers/profile_controller.go
+++ controllers/profile_controller.go
@@ -22,14 +22,19 @@ func NewProfileController() *ProfileController {
 // UploadPicture - Upload gambar profile
 // @Summary Upload gambar profile
 // @Tags Profile
+// @Security BearerAuth
 // @Accept multipart/form-data
 // @Produce json
 // @Param user_id path int true "User ID"
 // @Param profile_pic formData file true "Gambar"
 // @Success 200 {object} models.Response
 // @Failure 400 {object} models.Response
+// @Failure 401 {object} models.Response
 // @Router /upload/{user_id} [post]
 func (c *ProfileController) UploadPicture(ctx *gin.Context) {
+	authUserID, _ := ctx.Get("user_id")
+	authUsername, _ := ctx.Get("username")
+
 	userID, _ := strconv.Atoi(ctx.Param("user_id"))
 
 	file, err := ctx.FormFile("profile_pic")
@@ -53,19 +58,30 @@ func (c *ProfileController) UploadPicture(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, models.Response{
 		Status:  "success",
 		Message: "Upload berhasil",
-		Data:    models.ToProfileResponse(*profile),
+		Data: gin.H{
+			"uploaded_by": gin.H{
+				"user_id":  authUserID,
+				"username": authUsername,
+			},
+			"profile": models.ToProfileResponse(*profile),
+		},
 	})
 }
 
 // GetPicture - Lihat gambar profile
 // @Summary Lihat gambar profile
 // @Tags Profile
+// @Security BearerAuth
 // @Produce json
 // @Param user_id path int true "User ID"
 // @Success 200 {object} models.Response
+// @Failure 401 {object} models.Response
 // @Failure 404 {object} models.Response
 // @Router /upload/{user_id} [get]
 func (c *ProfileController) GetPicture(ctx *gin.Context) {
+	authUserID, _ := ctx.Get("user_id")
+	authUsername, _ := ctx.Get("username")
+
 	userID, _ := strconv.Atoi(ctx.Param("user_id"))
 
 	profile, err := c.service.GetPicture(userID)
@@ -79,6 +95,12 @@ func (c *ProfileController) GetPicture(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, models.Response{
 		Status: "success",
-		Data:   models.ToProfileResponse(*profile),
+		Data: gin.H{
+			"requested_by": gin.H{
+				"user_id":  authUserID,
+				"username": authUsername,
+			},
+			"profile": models.ToProfileResponse(*profile),
+		},
 	})
 }
